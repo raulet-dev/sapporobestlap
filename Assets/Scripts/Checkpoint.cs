2390679f4f1bb041b014f972fb997d3576f2ba36
Raul <rey_brujo@hotmail.com>
2021-05-15 15:24:52 +0900

    upload

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    public Material Green;
+    public Material Red;
+    public Material Yellow;
+    public int checkpointNumber = 0;
+    private Component[] checkMats;
+    GameObject checks;
+    private bool notTriggered = true;
+    private Transform next;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkMats = this.gameObject.GetComponentsInChildren<Renderer>();
+        ChangeColor(0);
+        checks = GameObject.Find("GameControl");
+        next = transform.GetChild(3);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (checks.GetComponent<GameRun>().getCheckPoint() == this.checkpointNumber)
+        {
+            ChangeColor(1);
+            next.gameObject.SetActive(true);
+            
+        } else if (checks.GetComponent<GameRun>().getCheckPoint() < this.checkpointNumber)
+        {
+            ChangeColor(0);
+            next.gameObject.SetActive(false);
+        } else
+        {
+            ChangeColor(2);
+            next.gameObject.SetActive(false);
+        }
+
+
+
+    }
+
+    void ChangeColor(int a)
+    {
+        switch (a)
+        {
+            case 1:
+                foreach (Renderer mats in checkMats)
+                {
+                    mats.material = Yellow;
+                }
+                break;
+            case 2:
+                foreach (Renderer mats in checkMats)
+                {
+                    mats.material = Green;
+                }
+                break;
+            default:
+                foreach (Renderer mats in checkMats)
+                {
+                    mats.material = Red;
+                }
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (notTriggered)
+        {
+            checks.GetComponent<GameRun>().nextCheckpoint();
+            this.notTriggered = false;
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/ControlTime.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+public class ControlTime : MonoBehaviour
+{
+    private bool start = false;
+    private Text chrono;
+    public int timer = 100;
+    private float timing = 0f;
+    private float timeScore = 0f;
+    private bool firstTime = true;
+    private int maxTimer = 5;
+    private bool pause = false;
+    private string[] scoresRead;
+    private float[] scores = new float[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chrono = GetComponent<Text>();
+        if (File.Exists("scores.sc")){
+
+        } else
+        {
+            ResetScore();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start)
+        {
+            if (timer > 0)
+            {
+                timing += 1 * Time.deltaTime;
+                timer = maxTimer - (int)timing;
+                chrono.alignment = TextAnchor.LowerRight;
+                chrono.text = timer.ToString();
+            } else
+            {
+                timeScore += 1 * Time.deltaTime;
+                chrono.text = "Time:\n" + timeScore.ToString();
+                chrono.alignment = TextAnchor.UpperLeft;
+                if (firstTime)
+                {
+                    GameObject.Find("Car").GetComponent<CarControllerFree>().Started();
+                }
+            }
+        } else
+        {
+            if (timer > 0)
+            {
+                chrono.alignment = TextAnchor.LowerRight;
+            } else
+            {
+                chrono.alignment = TextAnchor.MiddleCenter;
+            }
+        }
+    }
+
+    public void TimeStart()
+    {
+        this.start = true;
+        this.timing = 0f;
+        this.firstTime = true;
+        this.timeScore = 0f;
+        this.timer = 100;
+        this.pause = false;
+
+    }
+
+    public void TimeStop()
+    {
+        this.start = false;
+    }
+
+    public void TimePause(bool a)
+    {
+        this.start = a;
+    }
+
+    public void setScore()
+    {
+        int tmp = -1;
+        scoresRead = File.ReadAllLines("scores.sc");
+        for (int a = 0; a < 5; a++)
+        {
+            scores[a] = float.Parse(scoresRead[a]);
+        }
+
+        for (int a = 0; a < 5; a++)
+        {
+            if(timeScore < scores[a])
+            {
+                tmp = a;
+                break;
+            }
+        }
+        if (tmp != -1)
+        {
+           
+            if(tmp < 4)
+            {
+                for(int a = 3; a >= tmp; a--)
+                {
+                    scores[a+1] = scores[a];
+                }
+            }
+            scores[tmp] = timeScore;
+            for (int a = 0; a < 5; a++)
+            {
+                scoresRead[a] = scores[a].ToString();
+            }
+            File.WriteAllLines("scores.sc", scoresRead);
+            GameObject.Find("ScoresText").GetComponent<ScoreBoard>().ScoreUpdate();
+        }
+
+
+
+    }
+
+    public void ResetScore()
+    {
+        string[] newFile = new string[5];
+        for (int a = 0; a < 5; a++)
+        {
+            newFile[a] = "999999";
+        }
+        File.WriteAllLines("scores.sc", newFile);
+    }
+
+}
